2cd4405557aaaac968a0d8a990ba06592291c152
Dan Bissonnet <dan@danisadesigner.com>
2013-07-31 13:05:45 +0100

    Add .entry-title--link class

--- heading.php
+++ heading.php
@@ -12,6 +12,6 @@ use Spliced\Theme\Underscores as T;
 </header><!-- .entry-header -->
 <?php else : ?>
 <header class="entry-header">
-	<h1 class="entry-title"><a href="<?php the_permalink(); ?>" title="<?php echo esc_attr( sprintf( __( 'Permalink to %s', '_s' ), the_title_attribute( 'echo=0' ) ) ); ?>" rel="bookmark"><?php the_title(); ?></a></h1>
+	<h1 class="entry-title"><a class="entry-title--link" href="<?php the_permalink(); ?>" title="<?php echo esc_attr( sprintf( __( 'Permalink to %s', '_s' ), the_title_attribute( 'echo=0' ) ) ); ?>" rel="bookmark"><?php the_title(); ?></a></h1>
 </header><!-- .entry-header -->
 <?php endif ?>
