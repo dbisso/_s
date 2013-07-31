05c4ed82c736e1278deaf4990033e72ba87843c2
Dan Bissonnet <dan@danisadesigner.com>
2013-07-31 12:11:22 +0100

    Enqueue skip link focus fix script

--- lib/Spliced/Theme/Underscores/Frontend.php
+++ lib/Spliced/Theme/Underscores/Frontend.php
@@ -48,6 +48,7 @@ class Frontend {
 		// Default Stylesheet
 		wp_enqueue_style( 'style', get_stylesheet_directory_uri() . '/style.css', null, null );
 
+		wp_enqueue_script( 'skip-link-focus', get_template_directory_uri() . '/js/skip-link-focus-fix.js', null, null, true );
 		wp_enqueue_script( 'respond', 'http://cdnjs.cloudflare.com/ajax/libs/respond.js/1.1.0/respond.min.js', null, null, false );
 		// wp_enqueue_script( 'enquire', get_template_directory_uri() . '/js/enquire.js', null, null, true );
 		wp_enqueue_script( 'small-menu', get_template_directory_uri() . '/js/small-menu.js', array( 'jquery' ), microtime(), true );
