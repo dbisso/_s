9a29e7da316e248ec0539ca5e6ae7ce77d705590
Dan Bissonnet <dan@danisadesigner.com>
2014-01-03 15:13:07 +0000

    Ensure theme JS passes lint

--- js/script.js
+++ js/script.js
@@ -1,8 +1,8 @@
+/* global Modernizr: true */
 /**
  * Site wide Javascript goes here
  */
-;jQuery( function($) {
- 	if ( Modernizr.svg ) {
-		document.getElementsByTagName('html')[0].className = document.getElementsByTagName('html')[0].className.replace(/no-svg ?/,'');
-	}
-});
+if ( Modernizr.svg ) {
+	document.getElementsByTagName('html')[0].className = document.getElementsByTagName('html')[0].className.replace(/no-svg ?/,'');
+}
+
