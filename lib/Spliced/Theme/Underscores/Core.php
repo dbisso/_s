7e1b1b47ad23f6d19d82143fc62e59ff6c704f7c
Dan Bissonnet <dan@danisadesigner.com>
2015-04-22 17:49:44 +0100

    Use add_theme_support( 'title-tag' );

--- lib/Spliced/Theme/Underscores/Core.php
+++ lib/Spliced/Theme/Underscores/Core.php
@@ -55,6 +55,7 @@ class Core {
 		add_theme_support( 'automatic-feed-links' );
 		add_theme_support( 'post-thumbnails' );
 		add_theme_support( 'html5', array( 'comment-list', 'comment-form', 'search-form', 'gallery' ) );
+		add_theme_support( 'title-tag' );
 		// add_theme_support( 'custom-header' );
 		// add_theme_support( 'post-formats', array( 'aside', ... ) );
 
