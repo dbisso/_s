bb8f4303f0c8637733f03920ca41465116360bfa
Dan Bissonnet <dan@danisadesigner.com>
2015-04-22 13:50:02 +0100

    Use button for menu toggle for better a11y

--- header.php
+++ header.php
@@ -36,7 +36,7 @@ use Spliced\Theme\Underscores as T;
 				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
 				<p class="site-description"><?php bloginfo( 'description' ); ?></p>
 			</div>
-			<a class="menu-toggle main-menu-label" data-menu="site-navigation" href="#tertiary"><?php _e( 'Menu', '_s' ); ?></a>
+			<button class="menu-toggle main-menu-label" data-menu="site-navigation" href="#tertiary"><?php _e( 'Menu', '_s' ); ?></button>
 			<a class="skip-link screen-reader-text" href="#content"><?php _e( 'Skip to content', '_s' ); ?></a>
 			<nav id="site-navigation" class="main-navigation" role="navigation">
 				<!--
